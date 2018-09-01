30ceb7796fdf533b780c1f9fe583c524322527d3
Noah Rossi <me@noahrossi.com>
2018-09-01 19:30:50 -0400

    changed url to start at /

--- moe365/urls.py
+++ moe365/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('attendance/', include('attendance.urls')),
+    path('', include('attendance.urls')),
     path('admin/', admin.site.urls),
 ]
